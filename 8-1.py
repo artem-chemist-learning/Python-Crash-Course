cd8084cce52e9b113887220e6aeb38a195071863
Artem <lebedevfedora@gmail.com>
2021-03-18 17:23:08 +0300

    Lesson on functions

---
+++ 8-1.py
@@ -0,0 +1,29 @@
+def display_mssg(name = 'Art'):
+    """
+    Braggs about my python progress
+
+    Returns
+    -------
+    None.
+
+    """
+    print (name+"'s learnt about python functions today")
+
+def fav_book(book = 'Dune'):
+    """
+    Talks up your favourite book
+
+    Parameters
+    ----------
+    book : string, optional
+        Your favourite book. The default is 'Dune'.
+
+    Returns
+    -------
+    None.
+
+    """
+    print(book+' is indeed a great book')
+
+display_mssg()
+fav_book('Catch 22')
