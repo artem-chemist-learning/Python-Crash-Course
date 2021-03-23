c2f9110eb7ae28b5aad3c9cda77191e90e687ec4
Artem <lebedevfedora@gmail.com>
2021-03-23 16:23:23 +0300

    Final exercise on exceptions

--- 10-10.py
+++ 10-10.py
@@ -1,17 +1,27 @@
-def print_file(file_name):
+def count_word(file_name, word):
+    total = (False, 0)
+    sum_result = 0
     try:
         with open(file_name, 'r', encoding='utf8') as f:
             file = f.readlines()
     except FileNotFoundError:
-        pass
+        return total
     else:
         for line in file:
-            print(line.strip())
+            increment = line.lower().count(word)
+            sum_result+=increment
+        total = (True, sum_result)
+        return total
+            
 
 
-file_1 = 'C:/Users/Documents/Source Code/Python-Text-Book/Test files/Cats.txt'
-file_2 = 'C:/Users/artem/Documents/Source Code/Python-Text-Book/Test files/Dogs.txt'
+file_1 = 'C:/Users/artem/Documents/Source Code/Python-Text-Book/Test files/Anna Karenina.txt'
+file_2 = 'C:/Users/artem/Documents/Source Code/Python-Text-Book/Test files/The call of the wild.txt'
 files_to_read = [file_1, file_2]
 
 for file in files_to_read:
-    print_file(file)
+    word_to_count = 'dog'
+    total_count = count_word(file, word_to_count)
+    if total_count[0] == True:
+        str_count = str(total_count[1])
+        print('Total number of {} is {}'. format(word_to_count, str_count))
